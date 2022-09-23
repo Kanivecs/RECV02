9a82c606b8f92cba2e93e5821e4a6207fb4d0dfb
user <kanivecskristaps@gmail.com>
2022-09-23 11:31:55 +0300

    Git_uplad_05

--- linux_special_variables_test.sh
+++ linux_special_variables_test.sh
@@ -6,3 +6,9 @@ echo "Second Parameter : $2"
 echo "Quoted Values: $@"
 echo "Quoted Values: $* "
 echo "Total Number of Parameters : $#"
+
+#echo "Please, enter some text: "
+#read var_string
+#echo "You have entered $var_string"
+
+
